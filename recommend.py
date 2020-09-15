efc5394eb9888115fc87cf5658f98ced2f3ba75c
emmanuelnunez <emmanuel.nunez@summitmedia.com.ph>
2020-09-16 00:49:38 +0800

    add params

--- recommend.py
+++ recommend.py
@@ -1,11 +1,11 @@
 import os
+# from neural_recommender.collab_filtering import NeuralCollabFiltering
 from collab_filtering import NeuralCollabFiltering
-
 
 # Init
 base_dir = os.path.abspath(os.path.dirname(__file__))
 model_path = os.path.join(base_dir, 'model/spot_ncf_model_20200915')
-encoder_path = os.path.join(base_dir, 'model/encodings.json')
+encoder_path = os.path.join(base_dir, 'model/encodings_20200915.json')
 ref_path = os.path.join(base_dir, 'dataset/spot_scid_slug_20200301-20200831_reduced.csv')
 
 # Define column headers
@@ -14,5 +14,5 @@ cols = ['summit_client_id', 'slug', 'time_on_page']
 ncf = NeuralCollabFiltering(cols)
 ncf.load_model(ref_path, model_path, encoder_path)
 
-def reco(summit_id):
-    return ncf.recommend(summit_id)
+def reco(summit_id, **kwargs):
+    return ncf.recommend(summit_id, **kwargs)
